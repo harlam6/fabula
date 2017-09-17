f14191efee3a1cb8ac2f2918e674deea3fb13149
harlam6 <harlam6@users.noreply.github.com>
2017-09-17 15:15:30 -0700

    readded button for database

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ require("./routes/html-routes.js")(app);
 // =============================================================
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT 2000 " + PORT);
+    console.log("App listening on PORT " + PORT);
   });
 });
 
